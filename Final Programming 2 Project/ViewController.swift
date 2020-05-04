ace2421040ff9fb2fdf361d41beb2f6a13d1536d
Marlayna Verenna <m.verenna@setonhill.edu>
2020-05-04 13:25:13 -0400

    Updated!

--- Final Programming 2 Project/ViewController.swift
+++ Final Programming 2 Project/ViewController.swift
@@ -64,54 +64,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                }
     }
     
-    @IBOutlet weak var upVoteCounter: UILabel!
-    @IBOutlet weak var downVoteCounter: UILabel!
-    @IBOutlet weak var winner: UILabel!
-    
-    override func viewWillAppear (_ animated: Bool){
-        upVoteCounter.text = String((parent as! TabBarController).chocolateVote)
-        downVoteCounter.text = String((parent as! TabBarController).mintChipVote)
-        
-        if (parent as! TabBarController).chocolateVote > (parent as! TabBarController).mintChipVote
-        {
-            winner.text = "Chocolate!"
-        }else{
-            winner.text = "Mint Chip!"
-        }
-        
-    }
-    
-    @IBOutlet weak var imageView: UIImageView!
-    var originalRect: CGRect!
-    
-    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        if motion==UIEvent.EventSubtype.motionShake {
-               }
-       }
-    
-    @IBAction func pinch(_ sender: Any) {
-        var recognizer: UIPinchGestureRecognizer
-        var scale: CGFloat
-        
-        recognizer=sender as! UIPinchGestureRecognizer
-        scale=recognizer.scale
-        imageView.transform = CGAffineTransform(rotationAngle: 0.0)
-        
-        imageView.frame = CGRect(origin: CGPoint(x: self.originalRect.origin.x,y :originalRect.origin.y),size: CGSize(width: originalRect.size.width*scale, height: originalRect.size.height*scale))
-    }
-    
-    @IBAction func rotate(_ sender: Any) {
-        var recognizer: UIRotationGestureRecognizer
-        var rotation: CGFloat
-        
-        recognizer=sender as! UIRotationGestureRecognizer
-        rotation=recognizer.rotation
-        imageView.transform = CGAffineTransform(rotationAngle: 0.0)
-        imageView.transform = CGAffineTransform(rotationAngle: rotation)
-    }
-   
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -128,14 +80,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
              let myRequest = URLRequest(url: myURL!)
              webView.load(myRequest)
         
-        becomeFirstResponder()
-    
-        originalRect=imageView.frame;
-        var tempImageView: UIImageView
-        tempImageView=UIImageView(image:UIImage(named: "Ice Cream Clip Art.png"))
-        tempImageView.frame=originalRect
-        view.addSubview(tempImageView)
-        self.imageView=tempImageView
         
     }
 
